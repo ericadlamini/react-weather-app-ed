2d9fff183a7a076ba5acaa44b6caf5626301e622
ericadlamini <erica.thamie@gmail.com>
2024-02-25 18:35:33 +0200

    Updated Footer & Links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="footer">
+        This app was Coded by{" "}
+        <a
+          href="https://github.com/ericadlamini"
+          target="blank"
+          rel="noreferrer"
+        >
+          Erica Dlamini
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/ericadlamini/react-weather-app-ed"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://react-weather-app-ed.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </div>
     </div>
   );
 }
