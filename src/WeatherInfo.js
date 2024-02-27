fa9bb74fa3a17a33def83b9054bb17821a330715
ericadlamini <erica.thamie@gmail.com>
2024-02-27 13:30:53 +0200

    Created New Component for Weather Icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SubDate from "./SubDate";
+import WeatherIcon from "./WeatherIcon.js";
 
 export default function weatherInfo(props) {
   return (
@@ -13,10 +14,9 @@ export default function weatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
-            alt="Mostly Cloudy"
-          />
+          <span>
+            <WeatherIcon code={props.data.icon} />
+          </span>
           <span className="temperature">
             {" "}
             {Math.round(props.data.temperature)}
